f6abf9b99c000db51e74d781c9ac2981f80e50ff
Louis Leong <louisleong@users.noreply.github.com>
2019-10-11 11:56:09 +0800

    Update UnitTest1.cs

--- src/WebApplication1.Tests/UnitTest1.cs
+++ src/WebApplication1.Tests/UnitTest1.cs
@@ -7,7 +7,7 @@ namespace WebApplication1.Tests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1(
+        public void TestMethod1()
         {
             //testing
         }
