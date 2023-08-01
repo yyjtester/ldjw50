093f4a2268453628dc1c25372109ab6a5aa45412
chloesacil <108705292+chloesacil@users.noreply.github.com>
2023-08-01 03:27:31 +0000

    edited homecontroller to add connections n signup

--- WebAppProj/Controllers/HomeController.cs
+++ WebAppProj/Controllers/HomeController.cs
@@ -26,6 +26,14 @@ public class HomeController : Controller
     {
         return View();
     }
+        public IActionResult Connections()
+    {
+        return View();
+    }
+        public IActionResult Signup()
+    {
+        return View();
+    }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
