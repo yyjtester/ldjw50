3d22105d8d0cc5cf108fd753abcb18e52cc75853
20034695-Yong-YungJian <106300317+20034695-Yong-YungJian@users.noreply.github.com>
2023-08-03 15:43:47 +0000

    Create Food request food web page

--- WebAppProj/Controllers/HomeController.cs
+++ WebAppProj/Controllers/HomeController.cs
@@ -43,6 +43,11 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Request()
+    {
+        return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
