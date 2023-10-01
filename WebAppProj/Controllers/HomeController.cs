35a846ef03901728958f4eb185c6a8b0f817223b
yyjtester <141350814+yyjtester@users.noreply.github.com>
2023-10-01 09:08:18 +0000

    update

--- WebAppProj/Controllers/HomeController.cs
+++ WebAppProj/Controllers/HomeController.cs
@@ -136,7 +136,7 @@ public class HomeController : Controller
     {
         return View();
     }
-    public IActionResult Donations_D()
+    public IActionResult Skill()
     {
         return View();
     }
